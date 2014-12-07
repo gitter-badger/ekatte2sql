3ef7cd809cf413170868209441f1aab6c012f01c
Ivan Ivanov <ivan.ivanov@suricactus.com>
2014-12-07 04:30:19 +0200

    No idea what I'm commiting here LOL

--- test.pl
+++ test.pl
@@ -2,54 +2,52 @@
 
 use utf8;
 use strict;
-
 use Data::Dumper;
-use Spreadsheet::Read;
-use DBI;
+
+
 use JSON;
 use Getopt::Long;
-
-
-use lib ('/home/suricactus/work/ekatte2sql/lib/perl');
-use NSI::Query;
-
-
-binmode STDIN, ':utf8';
-binmode STDOUT, ':utf8';
-
-
-
-# print Dumper 
-# $result;
-
-
-
-# use constant {
-# 	ADM_TYPE_CITY => 1,
-# 	ADM_TYPE_MONASTERY => 2,
-# 	ADM_TYPE_VILLAGE => 3,
-# 	ADM_TYPE_PROVINCE => 5,
-# 	ADM_TYPE_MUNICIPILITY => 4,
-# };
-
 
 
 use AnyEvent;
 
 
-my $condvar = AnyEvent->condvar;
-my $nsiq = new NSI::Query;
+use lib ('./lib/perl');
+use NSI::Query;
 
-my $result = $nsiq->GetTreeOfProvinces($condvar)
+
+binmode STDIN, ':utf8';
+binmode STDOUT, ':utf8';
+binmode STDERR, ':utf8';
+
+
+my $ARGS = {
+	'date' => '',
+};
+
+
+GetOptions( 
+    "date=s" => \$$ARGS{date}, 
+);
+
+$$ARGS{date} = $$ARGS{date} || `date -Idate`;
+
+
+
+my $condvar = AnyEvent->condvar;
+my $nsiq = new NSI::Query({
+	skip_rows => 2
+	});
+
+my $result = $nsiq->GetTreeOfProvinces($$ARGS{date})
 	->then(sub {
-			print "1\n";
+			my ($res) = @_;
+			print to_json( $res, { pretty => 1 } );
 			$condvar->send;
 		}, sub {
-			print "2\n";
+			print "It's impossible to see this";
 			$condvar->send;
 		});
-
-
 
 
 $condvar->recv;
