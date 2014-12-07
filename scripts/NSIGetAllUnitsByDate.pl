11f80bcf1b5072f10d778e4d52ac185ad5995dff
Ivan Ivanov <ivan.ivanov@suricactus.com>
2014-12-07 04:29:34 +0200

    Ready to use

---
+++ scripts/NSIGetAllUnitsByDate.pl
@@ -0,0 +1,55 @@
+#!/usr/lib/perl
+
+use utf8;
+use strict;
+use Data::Dumper;
+
+
+use JSON;
+use Getopt::Long;
+
+
+use AnyEvent;
+
+
+use lib ('./lib/perl');
+use NSI::Query;
+
+
+binmode STDIN, ':utf8';
+binmode STDOUT, ':utf8';
+binmode STDERR, ':utf8';
+
+
+my $ARGS = {
+	'date' => '',
+};
+
+
+GetOptions( 
+    "date=s" => \$$ARGS{date}, 
+);
+
+$$ARGS{date} = $$ARGS{date} || `date -Idate`;
+
+die("Invalid date") unless($$ARGS{date} =~ /^\d{4}-\d{2}-\d{2}$/);
+print STDERR "Requesting for date $$ARGS{date}";
+
+
+my $condvar = AnyEvent->condvar;
+my $nsiq = new NSI::Query({
+	skip_rows => 2
+	});
+
+my $result = $nsiq->GetTreeOfProvinces($$ARGS{date})
+	->then(sub {
+			my ($res) = @_;
+			print to_json( $res, { pretty => 1 } );
+			$condvar->send;
+		}, sub {
+			print "It's impossible to see this";
+			$condvar->send;
+		});
+
+
+$condvar->recv;
